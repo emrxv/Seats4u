30d4eb7879e097df1ddb71cf243bb4d96062e89a
Emre Sunar <emresunar@autoreg-4840024.dyn.wpi.edu>
2023-11-27 12:09:29 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { layout } from './Layout.js'
 import { CreateVenue } from './controller/CreateVenue.js'
+import { DeleteVenue } from './controller/DeleteVenue.js'
+import { ListVenues } from './controller/ListVenues.js'
+import { CreateShow } from './controller/CreateShow.js'
+import './model.js';
 
 function App() {
   return (
@@ -13,73 +17,73 @@ function App() {
           <div style={layout.formColumn}>
             <div style={layout.formRow}>
               <label style={layout.label}>Name:</label>
-              <input style={layout.input} />
+              <input id = "venuecreatename" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Location:</label>
-              <input style={layout.input} />
+              <input id = "venuelocation" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Side Left:</label>
-              <input style={layout.input} />
+              <input id = "sideleft" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Center:</label>
-              <input style={layout.input} />
+              <input id = "center" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Side Right:</label>
-              <input style={layout.input} />
+              <input id = "sideright" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Password:</label>
-              <input style={layout.input} />
+              <input id = "managerpass" style={layout.input} />
             </div>
 
             <button style={layout.createVenueButton} onClick={(e) => CreateVenue()}>Create Venue</button>
             <div style={layout.formRow}>
               <label style={layout.label}>Name:</label>
-              <input style={layout.input} />
+              <input id = "venuedeletename" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Password:</label>
-              <input style={layout.input} />
+              <input id = "managerpass" style={layout.input} />
             </div>
-            <button style={layout.deleteVenueButton}>Delete Venue</button>
+            <button style={layout.deleteVenueButton} onClick={(e) => DeleteVenue()}>Delete Venue</button>
           </div>
           <div style={layout.buttonsColumn}>
             <div style={layout.formRow}>
               <label style={layout.label}>Venue Name:</label>
-              <input style={layout.input} />
+              <input id = "venuenamecreateshow" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Show Name:</label>
-              <input style={layout.input} />
+              <input id = "showname" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Date:</label>
-              <input style={layout.input} />
+              <input id = "date" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Time:</label>
-              <input style={layout.input} />
+              <input id = "time" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Price:</label>
-              <input style={layout.input} />
+              <input id = "price" style={layout.input} />
             </div>
             <div style={layout.formRow}>
               <label style={layout.label}>Password:</label>
-              <input style={layout.input} />
+              <input id = "showcreatepass" style={layout.input} />
             </div>
-            <button style={layout.createShowButton}>Create Show</button>
+            <button style={layout.createShowButton} onClick={(e) => CreateShow()}>Create Show</button>
           </div>
           <div style={layout.AdminColumn}>
             <div style={layout.formRow}>
               <label style={layout.label}>Password:</label>
-              <input style={layout.input} />
+              <input id = "adminpass" style={layout.input} />
             </div>
-            <button style={layout.listVenuesButton}>List Venues</button>
+            <button style={layout.listVenuesButton} onClick={(e) => ListVenues()}>List Venues</button>
 
 
           </div>
---
+++ src/controller/CreateVenue.js
@@ -0,0 +1,30 @@
+function VenueCreate() {
+    let venuename = document.getElementById("venuecreatename").value;
+    let location = document.getElementById("venuelocation").value;
+    let sideleft = document.getElementById("sideleft").value;
+    let center = document.getElementById("center").value;
+    let sideright = document.getElementById("sideright").value;
+    let managerpass = document.getElementById("managerpass").value;
+
+    let payload = {
+        "venuecreatename": venuename,
+        "venuelocation": location,
+        "sideleft": sideleft,
+        "center": center,
+        "sideright": sideright,
+        "managerpass": managerpass
+    }
+    const response = fetch("https://lus8hhd6fi.execute-api.us-east-1.amazonaws.com/seats4uSTAGE/venue",
+        {
+            method: "POST",
+            body: JSON.stringify(payload)
+        }).then((response) => response)
+
+    const fetchResult = async () => {
+        let val = await response
+        let result = await val.json()
+        document.getElementById("result").value = result.body
+    }
+
+    fetchResult()
+}
