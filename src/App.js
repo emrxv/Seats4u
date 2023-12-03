4049d41be26d004b676da12c8958bc67fdb701ad
Emre Sunar <emresunar@autoreg-4840024.dyn.wpi.edu>
2023-12-03 18:14:45 -0500

    generateshows+listactiveshows(WIP)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import React from 'react';
 import './App.css';
-import { layout } from './Layout.js'
-import { CreateVenue } from './controller/CreateVenue.js'
-import { DeleteVenue } from './controller/DeleteVenue.js'
-import { ListVenues } from './controller/ListVenues.js'
-import { CreateShow } from './controller/CreateShow.js'
-import './model.js';
-
-// this is a comment by Jess :)
+import { venueConfig, venueConfig_big} from './seatsConfig.js'
+import { Venue } from './model.js'
+import { redrawCanvas } from './Boundary.js'
+import sold from './sold.png'
 
 function App() {
+  const [venue, setModel] = React.useState(new Venue(venueConfig));
+  const [redraw, forceRedraw] = React.useState(0);       // used to conveniently request redraw after model change
+  const appRef = React.useRef(null);
+  const canvasRef = React.useRef(null);   // need to be able to refer to Canvas
 
+  /** Ensures initial rendering is performed, and that whenever model changes, it is re-rendered. */
+  React.useEffect (() => {
+    redrawCanvas(venue, canvasRef.current, appRef.current)
+    }, [venue, redraw])   // arguments that determine when to refresh
+
+  return ( 
+    <div className="App">
+      <canvas tabIndex="1"  
+        className="App-canvas"
+        ref={canvasRef}
+        width  = "1200"
+        height = "800"/>
+      <header className="App-header">
+      <img id="sold" src={sold} alt="hidden" hidden></img>
+      </header>
+    </div>
+  );
 }
-
 export default App;
--- src/model.js
+++ src/model.js
@@ -12,12 +12,13 @@ export class Block {
     constructor(blockID, price, seats) {
         this.blockID = blockID
         this.price = price
-       // this.seats = seats make array
+        this.seats = [];//seats make array
 
     }
 }
 
 export class Show {
+    /**
     constructor(name, proceeds, date, time, isActive, blocks, seatsAvailable, seatsRemaining) {
     this.name = name
     this.isActive = isActive
@@ -28,12 +29,20 @@ export class Show {
     let totalSeats = seatsAvailable + seatsRemaining;
     this.totalSeats = totalSeats
     }
+    */
+    constructor(config) {
+        this.initialize(config)
+    }
+    initialize(config) {
+
+    }
 }
 
 export class Venue {
+    /** 
     constructor(name, venueShows, location, sideLeft, center, sideRight, managerPass, adminPass) {
         this.name = name
-       // this.venueShows = venueShows make array
+        this.venueShows = []; //venueShows make array
         this.location = location
         this.sideLeft = sideLeft
         this.center = center
@@ -41,12 +50,43 @@ export class Venue {
         this.managerPass = managerPass
         this.adminPass = adminPass
 
-        this.Venue = Array.from(Array(33), () => new Array(33));
+        this.Venue = Array.from(Array(33), () => new Array(33));  
+    }
+    */
+    constructor(config) {
+        this.initialize(config)
+    }
+    initialize(config) {
         let ID = 0;
-        for (let r = 0; r < 33; r++) {
-            for (let c = 0; c < 33; c++) {
-                this.Venue[r][c] = new Seat(ID, )
-                ID++;
+    
+        for (let l = 0; l < 3; l++) {
+            let nr, nc;
+    
+            if (l === 0) { // sideLeft
+                nr = config.endRowColL.split("", 2)[0].charCodeAt(0) - 65;
+                nc = parseInt(config.endRowColL.split("", 2)[1]);
+                this.sideLeft = Array.from(Array(nr), () => new Array(nc));
+            } else if (l === 1) { // center
+                nr = config.endRowColC.split("", 2)[0].charCodeAt(0) - 65;
+                nc = parseInt(config.endRowColC.split("", 2)[1]);
+                this.center = Array.from(Array(nr), () => new Array(nc));
+            } else if (l === 2) { // sideRight
+                nr = config.endRowColR.split("", 2)[0].charCodeAt(0) - 65;
+                nc = parseInt(config.endRowColR.split("", 2)[1]);
+                this.sideRight = Array.from(Array(nr), () => new Array(nc));
+            }
+    
+            for (let r = 0; r < nr; r++) {
+                for (let c = 0; c < nc; c++) {
+                    if (l === 0) {
+                        this.sideLeft[r][c] = new Seat(ID, String.fromCharCode(r + 65), c+1, "sideLeft", 0);
+                    } else if (l === 1) {
+                        this.center[r][c] = new Seat(ID, String.fromCharCode(r + 65), c+1, "center", 0);
+                    } else if (l === 2) {
+                        this.sideRight[r][c] = new Seat(ID, String.fromCharCode(r + 65), c+1, "sideRight", 0);
+                    }
+                    ID++;
+                }
             }
         }
     }
